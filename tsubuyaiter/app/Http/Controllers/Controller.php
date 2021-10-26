2f4c1cd512888f6433f6839d03623b13a345c239
i-icc <ichi623110@gmail.com>
2021-10-26 14:35:15 +0900

    #4 ユーザー登録

--- tsubuyaiter/app/Http/Controllers/Controller.php
+++ tsubuyaiter/app/Http/Controllers/Controller.php
@@ -6,15 +6,40 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use App\User;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use \Symfony\Component\HttpFoundation\Response;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function signup(Request $request) {
-        return "test";
+    public function signup(Request $request)
+    {
+        /** @var Illuminate\Validation\Validator $validator */
+        $validator = Validator::make($request->all(), [
+            'user_name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->messages(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $user = User::create([
+            'user_name' =>  $request->user_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        $json = [
+            'data' => $user,
+            'message' => 'ユーザー登録完了',
+            'error' => ''
+        ];
+
+        return response()->json($json, Response::HTTP_OK);
     }
 }
-
