13c668cf64b478c056af8dd2f6ac56bf9d0b11a1
i-icc <ichi623110@gmail.com>
2021-10-27 12:36:20 +0900

    #6 ルーティング

--- tsubuyaiter/routes/api.php
+++ tsubuyaiter/routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\SignupController;
 use App\Http\Controllers\SigninController;
+use App\Http\Controllers\MessageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +26,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/v1/signup',[SignupController::class, 'signup']);
 
 Route::post('/v1/signin',[SigninController::class, 'signin']);
+
+Route::post('/v1/messages',[MessageController::class, 'postMessage']);
