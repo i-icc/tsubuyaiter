01fb48c4e366e0bf74cf2522d5bfccdac1001c84
i-icc <ichi623110@gmail.com>
2021-10-27 15:07:45 +0900

    #6 create message request

---
+++ tsubuyaiter/app/Http/Requests/Message.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+
+class Message extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'message' => 'required'
+        ];
+    }
+
+        /**
+     * [override] バリデーション失敗時のハンドリング
+     *
+     * @param Validator $validator
+     * @throw HttpResponseException
+     * @see FormRequest::failedValidation()
+     */
+    protected function failedValidation(Validator $validator) {
+        $response['status']  = 400;
+        $response['errors']  = $validator->errors();
+        throw new HttpResponseException(
+            response()->json( $response, 400 )
+        );
+    }
+}
